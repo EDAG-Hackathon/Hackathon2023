ef68a6dbff4d014fbf0e2363e31db787deaeab2a
Dietz, Samuel <samuel.dietz@edag.com>
2023-10-28 18:31:31 +0200

    use data from backend and add fetch hook

--- frontend/src/app/organisations/page.tsx
+++ frontend/src/app/organisations/page.tsx
@@ -2,116 +2,89 @@
 import Box from "@mui/material/Box";
 import Link from "next/link";
 import {
-    Avatar,
-    List,
-    ListItem,
-    ListItemAvatar,
-    ListItemText,
-    TextField,
-    Typography,
+  Avatar,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  TextField,
+  Typography,
 } from "@mui/material";
 import Map from "@/components/map";
 import ImageIcon from "@mui/icons-material/Image";
 import Divider from "@mui/material/Divider";
-import {useState} from "react";
-import {bgcolor} from "@mui/system";
+import { useState } from "react";
+
+import { useFetch } from "@/hooks/use-fetch";
+
+type Organisation = {
+  id: string;
+  name: string;
+  address: string;
+  image?: string;
+  coordinates: {
+    lat: number;
+    lng: number;
+  };
+};
 
 export default function Page() {
-    const [searchQuery, setSearchQuery] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
 
-    // Mocked api response
-    const organisations = [
-        {
-            id: "43459e1e-7d12-4e70-9d8d-5bf7eee890bd",
-            name: "Hochschule Fulda",
-            address: "Leipziger Str. 124",
-        },
-        {
-            id: "53659e1e-7d12-4e70-9d8d-5bf7eee890bd",
-            name: "Testschule",
-            address: "Bonifatiusstraße 1",
-        },
-        {
-            id: "53659e1e-7d12-4e70-9d8d-5bf7eee890bd",
-            name: "Bonifatiusschulej",
-            address: "Bonifatiusstraße 8",
-        },
-        {
-            id: "53659e1e-7d12-4e70-9d8d-5bf7eee890bd",
-            name: "Heinrich-von-Bibra-Schule",
-            address: "Heinrich-von-Bibra-Platz 1",
-        },
-        {
-            id: "53659e1e-7d12-4e70-9d8d-5bf7eee890bd",
-            name: "Schule am Rosenbad",
-            address: "Rosenbadstraße 1",
-        },
-        {
-            id: "53659e1e-7d12-4e70-9d8d-5bf7eee890bd",
-            name: "Bonifatiusschulej",
-            address: "Bonifatiusstraße 8",
-        },
-        {
-            id: "53659e1e-7d12-4e70-9d8d-5bf7eee890bd",
-            name: "Heinrich-von-Bibra-Schule",
-            address: "Heinrich-von-Bibra-Platz 1",
-        },
-        {
-            id: "53659e1e-7d12-4e70-9d8d-5bf7eee890bd",
-            name: "Schule am Rosenbad",
-            address: "Rosenbadstraße 1",
-        },
-    ];
+  const { data, error, isLoading } = useFetch<Organisation[]>(
+    "http://localhost:8000/api/organisations"
+  );
+  const organisations = data || [];
 
-    const filteredOrganisations = organisations.filter((org) =>
-        org.name.toLowerCase().includes(searchQuery.toLowerCase())
-    );
+  const filteredOrganisations = organisations.filter((org) =>
+    org.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
-    return (
-        <main className="flex min-h-screen flex-col items-center">
-            <Box sx={{height: "50vh", width: "100%"}}>
-                <Map/>
-            </Box>
-            <Box
-                sx={{
-                    height: "50vh",
-                    maxHeight: "50vh",
-                    width: "100%",
-                    color: "primary.main",
-                    overflow: "scroll",
-                }}
+  return (
+    <main className="flex min-h-screen flex-col items-center">
+      <Box sx={{ height: "50vh", width: "100%" }}>
+        <Map />
+      </Box>
+      <Box
+        sx={{
+          height: "50vh",
+          maxHeight: "50vh",
+          width: "100%",
+          color: "primary.main",
+          overflow: "scroll",
+        }}
+      >
+        <Box sx={{ position: "sticky", top: 0, zIndex: 1, bgcolor: "white" }}>
+          <Typography variant="h4">Organisationen</Typography>
+          <TextField
+            id="input-with-sx"
+            label="Search"
+            variant="outlined"
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+        </Box>
+        <List>
+          {filteredOrganisations.map((organisation) => (
+            <Link
+              key={organisation.id}
+              href={`organisations/${organisation.id}/buildings`}
             >
-                <Box sx={{position: "sticky", top: 0, zIndex: 1, bgcolor: "white"}}>
-                    <Typography variant="h4">Organisationen</Typography>
-                    <TextField
-                        id="input-with-sx"
-                        label="Search"
-                        variant="outlined"
-                        onChange={(e) => setSearchQuery(e.target.value)}
-                    />
-                </Box>
-                <List>
-                    {filteredOrganisations.map((organisation) => (
-                        <Link
-                            key={organisation.id}
-                            href={`organisations/${organisation.id}/buildings`}
-                        >
-                            <ListItem>
-                                <ListItemAvatar>
-                                    <Avatar>
-                                        <ImageIcon/>
-                                    </Avatar>
-                                </ListItemAvatar>
-                                <ListItemText
-                                    primary={organisation.name}
-                                    secondary={organisation.address}
-                                />
-                            </ListItem>
-                            <Divider/>
-                        </Link>
-                    ))}
-                </List>
-            </Box>
-        </main>
-    );
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    <ImageIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary={organisation.name}
+                  secondary={organisation.address}
+                />
+              </ListItem>
+              <Divider />
+            </Link>
+          ))}
+        </List>
+      </Box>
+    </main>
+  );
 }
