154691601d4db5b7308cc279e206532975d829fc
Dominik Mack <dominik.mack@icloud.com>
2023-10-28 16:05:43 +0200

    Fix postgres setup

--- backend/sql/postgres_init.sql
+++ backend/sql/postgres_init.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS organizations (
     name VARCHAR(255) NOT NULL,
     image VARCHAR(255),
     coordinates json NOT NULL,
-    address: VARCHAR(255)
+    address VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS buildings (
