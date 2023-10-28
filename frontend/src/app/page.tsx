c68a1afd7ac29be847b342396fc889cccbc5b892
Johannes Jobst <jobst_j@t-online.de>
2023-10-28 14:32:22 +0200

    testing

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 import Image from "next/image";
-import Calendar from "./calendar";
 import { CreateEditEventDialog } from "./create-edit-event-dialog";
+import { Calendar } from "./calendar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <CreateEditEventDialog/>
+      <CreateEditEventDialog />
       <Calendar />
     </main>
   );
