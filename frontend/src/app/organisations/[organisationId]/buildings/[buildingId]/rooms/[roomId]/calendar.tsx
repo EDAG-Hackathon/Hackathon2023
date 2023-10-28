a644b8b94cf9f557300c027ba7e8b2c63c8aa990
Johannes Jobst <jobst_j@t-online.de>
2023-10-28 18:36:26 +0200

    define calendar type

--- frontend/src/app/organisations/[organisationId]/buildings/[buildingId]/rooms/[roomId]/calendar.tsx
+++ frontend/src/app/organisations/[organisationId]/buildings/[buildingId]/rooms/[roomId]/calendar.tsx
@@ -5,12 +5,54 @@ import interactionPlugin from "@fullcalendar/interaction";
 import { CreateEditEventDialog } from "./create-edit-event-dialog";
 import { Room } from "./page";
 
+type Appointment = {
+  id: string;
+  title: string;
+  start: Date;
+  end: Date;
+  editable: boolean;
+  extendedProps: {
+    temperature: number;
+    ventilation: boolean;
+  };
+};
+
 export function Calendar(params: { selectedRoom: Room }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const [events, setEvents] = useState([
-    { title: "event 1", date: "2023-10-30" },
-    { title: "event 2", date: "2023-10-29" },
+  const [events, setEvents] = useState<Appointment[]>([
+    {
+      id: "1",
+      title: "Test",
+      start: new Date("2023-10-27T10:00:00"),
+      end: new Date("2023-10-27T12:00:00"),
+      editable: false,
+      extendedProps: { temperature: 20, ventilation: true },
+    },
+    {
+      id: "2",
+      title: "Test2",
+      start: new Date("2023-10-28T14:00:00"),
+      end: new Date("2023-10-28T15:00:00"),
+      editable: false,
+      extendedProps: { temperature: 18, ventilation: false },
+    },
+    {
+      id: "3",
+      title: "Test3",
+      start: new Date("2023-10-29T16:00:00"),
+      end: new Date("2023-10-29T18:00:00"),
+      editable: false,
+      extendedProps: { temperature: 20, ventilation: true },
+    },
+    {
+      id: "4",
+      title: "Test4",
+      start: new Date("2023-10-30T09:00:00"),
+      end: new Date("2023-10-30T10:00:00"),
+      editable: false,
+      extendedProps: { temperature: 20, ventilation: false },
+    },
   ]);
 
   function handleSelect() {
@@ -29,7 +71,6 @@ export function Calendar(params: { selectedRoom: Room }) {
         onClose={handleModalClose}
       />
       <FullCalendar
-        editable
         selectable
         events={events}
         select={handleSelect}
