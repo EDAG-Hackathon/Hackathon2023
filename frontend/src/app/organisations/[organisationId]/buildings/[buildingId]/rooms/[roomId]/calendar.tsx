7ca6ea99364f330f1c4147b8f0092839cdbb92f9
Johannes Jobst <jobst_j@t-online.de>
2023-10-28 21:07:47 +0200

    update calendar

--- frontend/src/app/organisations/[organisationId]/buildings/[buildingId]/rooms/[roomId]/calendar.tsx
+++ frontend/src/app/organisations/[organisationId]/buildings/[buildingId]/rooms/[roomId]/calendar.tsx
@@ -34,7 +34,40 @@ export function Calendar(params: { selectedRoom: Room }) {
   const { data, error, isLoading } = useFetch<Appointment[]>(
     `http://localhost:8000/api/appointments?roomId=${params.selectedRoom.id}`
   );
-  const appointments = data || [];
+  const appointments = data || [
+    {
+      id: "1",
+      title: "Test",
+      start: new Date("2023-10-27T10:00:00"),
+      end: new Date("2023-10-27T12:00:00"),
+      editable: false,
+      extendedProps: { temperature: 20, ventilation: true },
+    },
+    {
+      id: "2",
+      title: "Test2",
+      start: new Date("2023-10-28T14:00:00"),
+      end: new Date("2023-10-28T15:00:00"),
+      editable: false,
+      extendedProps: { temperature: 18, ventilation: false },
+    },
+    {
+      id: "3",
+      title: "Test3",
+      start: new Date("2023-10-29T16:00:00"),
+      end: new Date("2023-10-29T18:00:00"),
+      editable: false,
+      extendedProps: { temperature: 20, ventilation: true },
+    },
+    {
+      id: "4",
+      title: "Test4",
+      start: new Date("2023-10-30T09:00:00"),
+      end: new Date("2023-10-30T10:00:00"),
+      editable: false,
+      extendedProps: { temperature: 20, ventilation: false },
+    },
+  ];
 
   return (
     <div>
@@ -71,6 +104,10 @@ export function Calendar(params: { selectedRoom: Room }) {
           initialView={"timeGridWeek"}
           locale={"de"}
           nowIndicator={true}
+          allDaySlot={false}
+          slotDuration={"01:00:00"}
+          height={"auto"}
+          firstDay={1}
         />
       </Stack>
     </div>
