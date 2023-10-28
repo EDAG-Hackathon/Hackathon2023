3a112b47afa20f9fa5121cc60a56539024b9e11d
Johannes Jobst <jobst_j@t-online.de>
2023-10-28 12:13:23 +0200

    create component

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withTM = require("next-transpile-modules")([
+  "@fullcalendar/common",
+  "@babel/preset-react",
+  "@fullcalendar/common",
+  "@fullcalendar/daygrid",
+  "@fullcalendar/interaction",
+  "@fullcalendar/react",
+  "@fullcalendar/timegrid",
+]);
 
-module.exports = nextConfig
+module.exports = withTM({
+  // your custom config goes here
+});
---
+++ frontend/src/app/calendar.tsx
@@ -0,0 +1,24 @@
+// components/Calendar.tsx
+import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+
+function Calendar() {
+  return (
+    <div>
+      <h1>Mein Kalender</h1>
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        events={
+          [
+            // Hier können Sie Ihre Ereignisse definieren
+            // Beispiel: { title: 'Event 1', date: '2023-10-28' },
+          ]
+        }
+      />
+    </div>
+  );
+}
+
+export default Calendar;
