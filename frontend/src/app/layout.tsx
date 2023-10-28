074a72310753c19de45422ddf2860096cf57dcb2
Marvin Spiegel <marvin.spiegel@googlemail.com>
2023-10-28 16:16:52 +0200

    change adapter locale to de

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import 'dayjs/locale/de';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
+    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="de">
       <html lang="de">
         <body className={inter.className}>{children}</body>
       </html>
