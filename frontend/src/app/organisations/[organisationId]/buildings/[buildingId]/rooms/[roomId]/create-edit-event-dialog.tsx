187155a00d7623b76b009b4df9e30ed68eda8e62
Johannes Jobst <jobst_j@t-online.de>
2023-10-28 17:30:31 +0200

    change params

--- frontend/src/app/organisations/[organisationId]/buildings/[buildingId]/rooms/[roomId]/create-edit-event-dialog.tsx
+++ frontend/src/app/organisations/[organisationId]/buildings/[buildingId]/rooms/[roomId]/create-edit-event-dialog.tsx
@@ -11,39 +11,34 @@ import {
 } from "@mui/material";
 import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker/DateTimePicker";
 import { useEffect, useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 
-
-type CreateEditEventDialogProps = {
-  id: string
+export function CreateEditEventDialog(params: {
+  id: string;
   isModalOpen: boolean;
   onClose: () => void;
-};
-
-
-export function CreateEditEventDialog(props: CreateEditEventDialogProps) {
-  const { id, isModalOpen = false, onClose } = props;
+}) {
+  const { id, isModalOpen = false, onClose } = params;
 
   const [selectedRoom, setSelectedRoom] = useState("Demoroom");
   const [selectedDate, setSelectedDate] = useState(new Date());
-  
+
   useEffect(() => {
-    axios.get("localhost:8000/api/rooms/" + id)
-      .then(response => {
-        setSelectedRoom(response.data)
+    axios
+      .get("localhost:8000/api/rooms/" + id)
+      .then((response) => {
+        setSelectedRoom(response.data);
       })
-      .catch(error => {
-        console.error('Failed to fetch', error);
+      .catch((error) => {
+        console.error("Failed to fetch", error);
       });
-  }, [])
+  }, []);
 
   const handleSave = () => {
     //API CALL
     console.log("save");
     onClose();
   };
-
-  
 
   return (
     <div>
@@ -62,22 +57,16 @@ export function CreateEditEventDialog(props: CreateEditEventDialogProps) {
             <Select
               id="event-room"
               label="Raum"
-              value={selectedRoom.name}
+              value={selectedRoom}
               fullWidth
               variant="outlined"
               disabled
               sx={{ mt: 1, mb: 1 }}
             >
-              <MenuItem value={selectedRoom.name}>{selectedRoom.name}</MenuItem>
+              <MenuItem value={selectedRoom}>{selectedRoom}</MenuItem>
             </Select>
-            <DateTimePicker 
-              label="Beginn"
-              sx={{ mt: 1, mb: 1 }} 
-            />
-            <DateTimePicker 
-              label="Ende" 
-              sx={{ mt: 1 }}
-            />
+            <DateTimePicker label="Beginn" sx={{ mt: 1, mb: 1 }} />
+            <DateTimePicker label="Ende" sx={{ mt: 1 }} />
           </FormControl>
         </DialogContent>
         <DialogActions>
