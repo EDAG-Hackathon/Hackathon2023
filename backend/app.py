d0b77cb542b37de936e749af093a484ba47a8a13
Dominik Mack <dominik.mack@icloud.com>
2023-10-29 09:42:13 +0100

    Register events route

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,7 @@ from api.api_organisations import api as api_organisations
 from api.api_rooms import api as api_rooms
 from api.api_appointments import api as api_appointments
 from api.api_swaggerui import api as api_swaggerui
+from api.api_events import api as api_events
 from util.mock_data import generate_mock_data
 
 app = Chalice("dashboard-backend")
@@ -22,5 +23,6 @@ app.register_blueprint(api_buildings, url_prefix="/api")
 app.register_blueprint(api_rooms, url_prefix="/api")
 app.register_blueprint(api_appointments, url_prefix="/api")
 app.register_blueprint(api_swaggerui, url_prefix="/api")
+app.register_blueprint(api_events, url_prefix="/api")
 
 generate_mock_data()
