8203f9170dad0e74ae6004991aca914ce88f9881
Dietz, Samuel <samuel.dietz@edag.com>
2023-10-29 08:35:33 +0100

    add markers to map

---
+++ frontend/src/components/markers/organisation-marker.tsx
@@ -0,0 +1,86 @@
+import { Marker } from "react-map-gl";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, CardActions } from "@mui/material";
+import PlaceIcon from "@mui/icons-material/Place";
+
+import { Organisation } from "@/app/organisations/page";
+import { useState } from "react";
+
+export default function OrganisationMarker({
+  organisation,
+}: {
+  organisation: Organisation;
+}) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Marker
+      longitude={organisation.coordinates.lng}
+      latitude={organisation.coordinates.lat}
+      anchor="bottom"
+    >
+      {open ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "column",
+            alignItems: "center",
+            position: "relative",
+            top: "27px",
+          }}
+        >
+          <Card sx={{ maxWidth: 370 }}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="140"
+                image={organisation.image}
+                alt="green iguana"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {organisation.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Lizards are a widespread group of squamate reptiles, with over
+                  6,000 species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button size="small" color="primary">
+                Gebäude anzeigen
+              </Button>
+            </CardActions>
+          </Card>
+          <PlaceIcon
+            sx={{
+              //   color: "white",
+              stroke: "white",
+              fontSize: 64,
+              cursor: "pointer",
+              position: "relative",
+              zIndex: -1,
+              top: "-32px",
+            }}
+            onClick={() => setOpen(true)}
+          />
+        </div>
+      ) : (
+        <PlaceIcon
+          sx={{
+            //   color: "white",
+            stroke: "white",
+            fontSize: 64,
+            cursor: "pointer",
+          }}
+          onClick={() => setOpen(true)}
+        />
+      )}
+    </Marker>
+  );
+}
